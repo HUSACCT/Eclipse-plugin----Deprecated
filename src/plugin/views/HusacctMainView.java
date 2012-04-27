91ec90c9df5a8dccef702674b1c12b261b4e8a00
Tim.Linschoten <linschoten_99@hotmail.com>
2012-04-27 13:41:29 +0200

    Added the table to the MainView

--- src/plugin/views/HusacctMainView.java
+++ src/plugin/views/HusacctMainView.java
@@ -1,19 +1,31 @@
 package plugin.views;
 
+import husacct.Main;
 import husacct.ServiceProvider;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Dimension;
 import java.awt.Frame;
-import java.awt.Panel;
+import java.awt.Label;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JInternalFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
-public class HusacctMainView extends ViewPart {
-
+public class HusacctMainView extends ViewPart {	
+		JInternalFrame jif;
+		Frame frame;
+		ServiceProvider sp = ServiceProvider.getInstance();
 	public HusacctMainView() {
 		
 	}
@@ -21,11 +33,38 @@ public class HusacctMainView extends ViewPart {
 	@Override
 	public void createPartControl(Composite parent) {	
 		Composite composite = new Composite(parent, SWT.EMBEDDED);
-		Frame frame = SWT_AWT.new_Frame(composite);
-		ServiceProvider sp = ServiceProvider.getInstance();
-		JInternalFrame jif = sp.getValidateService().getBrowseViolationsGUI();
-		frame.add(jif.getRootPane());		
-		parent.setParent(composite);		
+		
+		jif = sp.getValidateService().getBrowseViolationsGUI();
+		frame = SWT_AWT.new_Frame(composite);
+		BorderLayout bl = new BorderLayout();
+		frame.setLayout(bl);
+		frame.add(jif.getRootPane(), BorderLayout.CENTER);		
+		String[] columnNames = {"Action"}; 
+        Object[][] data = {
+        {"Open Project"},
+        {"Define"},
+        {"Analyse"},
+        {"Check Conformance"},
+        {"Test"}
+        };
+ 
+        final JTable table = new JTable(data, columnNames);
+        table.setPreferredScrollableViewportSize(new Dimension(200, 200));
+        table.setFillsViewportHeight(true);
+        table.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+               // printDebugData(table);
+            	int i = table.getSelectedRow();
+            	if (i == 1){
+            		System.out.println("JAAA");
+            		JInternalFrame jif2 = new JInternalFrame();
+            		jif2 = sp.getValidateService().getBrowseViolationsGUI();
+            		frame.add(jif2.getRootPane(), BorderLayout.CENTER);
+            	}            	
+            }
+        });
+        frame.add(table, BorderLayout.LINE_START);
+       	parent.setParent(composite);		
 	}
 
 	@Override
