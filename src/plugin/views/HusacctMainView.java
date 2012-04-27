15b4b4c199b0a32ff8b46117d6cba648906524ef
stefan89 <stefan89_@hotmail.com>
2012-04-27 15:30:18 +0200

    HUSACCT Main View Update

--- src/plugin/views/HusacctMainView.java
+++ src/plugin/views/HusacctMainView.java
@@ -4,7 +4,6 @@ import husacct.Main;
 import husacct.ServiceProvider;
 import husacct.control.presentation.MainGui;
 import husacct.control.task.MainController;
-
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Dimension;
@@ -12,11 +11,10 @@ import java.awt.Frame;
 import java.awt.Label;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
+import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.eclipse.swt.SWT;
@@ -24,50 +22,80 @@ import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
+//jif2 = sp.getGraphicsService().getAnalysedArchitectureGUI();
 public class HusacctMainView extends ViewPart {	
-		JInternalFrame jif;
-		Frame frame;
-		ServiceProvider sp = ServiceProvider.getInstance();
-		
+
+	JInternalFrame JInternalFrameValidate;
+	JInternalFrame JInternalFrameDefine;
+
+	Frame frame;
+	ServiceProvider serviceProvider = ServiceProvider.getInstance();
+
 	public HusacctMainView() {
-		
+
 	}
 
 	@Override
 	public void createPartControl(Composite parent) {	
+
 		Composite composite = new Composite(parent, SWT.EMBEDDED);		
-		jif = sp.getValidateService().getBrowseViolationsGUI();
+		JInternalFrameValidate = serviceProvider.getValidateService().getBrowseViolationsGUI();
+		JInternalFrameDefine = serviceProvider.getDefineService().getDefinedGUI();
+		
+		
 		frame = SWT_AWT.new_Frame(composite);
-		BorderLayout bl = new BorderLayout();
-		frame.setLayout(bl);
-		frame.add(jif.getRootPane(), BorderLayout.CENTER);		
-		String[] columnNames = {"Action"}; 
-        Object[][] data = {
-        {"Open Project"},
-        {"Define"},
-        {"Analyse"},
-        {"Check Conformance"},
-        {"Test"}
-        }; 
-        final JTable table = new JTable(data, columnNames);
-        table.setPreferredScrollableViewportSize(new Dimension(200, 200));
-        table.setFillsViewportHeight(true);
-        
-        table.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-            	int i = table.getSelectedRow();
-            	if (i == 1){
-            		
-            	}            	
-            }
-        });
-        frame.add(table, BorderLayout.LINE_START);
-       	parent.setParent(composite);	
+		BorderLayout borderLayout = new BorderLayout();
+		frame.setLayout(borderLayout);
+		frame.add(JInternalFrameValidate.getRootPane(), BorderLayout.CENTER);		
+
+		String[] columnNames = {"Actions"}; 
+		Object[][] data = {
+				{"Select Source"},
+				{"Define Architecture"},
+				{"Import Architecture"},
+				{"Validate"},
+		}; 
+
+		final JTable jTable = new JTable(data, columnNames);
+		jTable.setPreferredScrollableViewportSize(new Dimension(300, 300));
+		jTable.setFillsViewportHeight(true);
+
+		jTable.addMouseListener(new MouseAdapter() {
+
+			public void mouseClicked(MouseEvent e) {
+				int column = jTable.getSelectedRow();
+				if (column == 0){ //Select source
+					//frame.add(jif2.getRootPane(), BorderLayout.CENTER);
+					Repaint();
+				
+				}
+				else if(column == 1){//Define Architecture
+					frame.add(JInternalFrameDefine.getRootPane(), BorderLayout.CENTER);
+					Repaint();
+
+				}
+				else if(column == 2){//Import Architecture   
+					//frame.add(jif1.getRootPane(), BorderLayout.CENTER);
+					Repaint();
+				}
+				else if(column == 3){//Validate  
+					frame.add(JInternalFrameValidate.getRootPane(), BorderLayout.CENTER);
+					Repaint();
+				}
+			}
+		});
+		frame.add(jTable, BorderLayout.LINE_START);
+		parent.setParent(composite);	
 	}
 
 	@Override
 	public void setFocus() {
 
 	}
-
+		
+	public void Repaint(){
+		
+		frame.validate();
+		frame.repaint();
+	}
 }
