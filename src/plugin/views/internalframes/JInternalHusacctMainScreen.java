e5b4d8c55e1cf555f81d5ed3faeec4aa84474544
Tim.Linschoten <linschoten_99@hotmail.com>
2012-05-03 16:26:29 +0200

    changed the picture to relative path and deleted not needed files

--- src/plugin/views/internalframes/JInternalHusacctMainScreen.java
+++ src/plugin/views/internalframes/JInternalHusacctMainScreen.java
@@ -8,13 +8,18 @@ import javax.swing.ImageIcon;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.jface.resource.ImageDescriptor;
+
+import plugin.Activator;
+
 public class JInternalHusacctMainScreen extends JInternalFrame {
 
 	public JInternalHusacctMainScreen() {
-		
 		BufferedImage myPicture;
 		try {
-			myPicture = ImageIO.read(new File("C:\\husacct.png"));
+			final String husacctpng = FileLocator.toFileURL(Activator.getDefault().getBundle().getEntry("icons/husacct.png")).getPath();
+			myPicture = ImageIO.read(new File(husacctpng));
 			JLabel picLabel = new JLabel(new ImageIcon( myPicture ));
 			getContentPane().add(picLabel);
 			
