84646331426f53bffe0c011d582592a73908dd5f
stefan89 <stefan89_@hotmail.com>
2012-05-10 21:35:35 +0200

    Select source functionaliteit in package explorer bijna klaar

--- src/plugin/builder/ToggleNatureAction.java
+++ src/plugin/builder/ToggleNatureAction.java
@@ -1,44 +1,44 @@
 package plugin.builder;
 
 import java.util.Iterator;
-
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import plugin.controller.PluginController;
 
-public class ToggleNatureAction implements IObjectActionDelegate {
+
+public class ToggleNatureAction implements IObjectActionDelegate    {
 
 	private ISelection selection;
-
+	private PluginController pluginController;
+	
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
 	 */
 	public void run(IAction action) {
-		System.out.println("Ja hoor, het knopje is ingedrukt! Stefan ook weer blij");
-//		if (selection instanceof IStructuredSelection) {
-//			for (Iterator it = ((IStructuredSelection) selection).iterator(); it
-//					.hasNext();) {
-//				Object element = it.next();
-//				IProject project = null;
-//				if (element instanceof IProject) {
-//					project = (IProject) element;
-//				} else if (element instanceof IAdaptable) {
-//					project = (IProject) ((IAdaptable) element)
-//							.getAdapter(IProject.class);
-//				}
-//				if (project != null) {
-//					toggleNature(project);
-//				}
-//			}
-//		}
+		System.out.println("Project path determined");
+		
+		if (selection instanceof IStructuredSelection) {
+			for (Iterator it = ((IStructuredSelection) selection).iterator(); it.hasNext();) {
+				Object element = it.next();
+				IProject project = null;
+				if (element instanceof IProject) {
+					project = (IProject) element;
+				} else if (element instanceof IAdaptable) {
+					project = (IProject) ((IAdaptable) element).getAdapter(IProject.class);
+				}
+				if (project != null) {
+					toggleNature(project);
+				}
+			}
+		}
 	}
 
 	/*
@@ -67,31 +67,12 @@ public class ToggleNatureAction implements IObjectActionDelegate {
 	 *            to have sample nature added or removed
 	 */
 	private void toggleNature(IProject project) {
-//		try {
-//			IProjectDescription description = project.getDescription();
-//			String[] natures = description.getNatureIds();
-//
-//			for (int i = 0; i < natures.length; ++i) {
-//				if (AnalyseNature.NATURE_ID.equals(natures[i])) {
-//					// Remove the nature
-//					String[] newNatures = new String[natures.length - 1];
-//					System.arraycopy(natures, 0, newNatures, 0, i);
-//					System.arraycopy(natures, i + 1, newNatures, i,
-//							natures.length - i - 1);
-//					description.setNatureIds(newNatures);
-//					project.setDescription(description, null);
-//					return;
-//				}
-//			}
-//
-//			// Add the nature
-//			String[] newNatures = new String[natures.length + 1];
-//			System.arraycopy(natures, 0, newNatures, 0, natures.length);
-//			newNatures[natures.length] = AnalyseNature.NATURE_ID;
-//			description.setNatureIds(newNatures);
-//			project.setDescription(description, null);
-//		} catch (CoreException e) {
-//		}
-	}
 
+			IPath projectPath = project.getLocation();
+			System.out.println(projectPath);
+
+		//	pluginController.sourceSelected(new String[]{projectPath.toString()}, "1.0");
+		//Hier moet de aanroep!!!!!!!!!!!!!!!!!!!	
+	}
+	
 }
