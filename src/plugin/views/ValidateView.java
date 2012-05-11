87299bf4597df6955934bc0c42c7f629ec753798
NielsNijveldt <nielsnijveldt@hotmail.com>
2012-05-11 15:17:26 +0200

    validateview updated

--- src/plugin/views/ValidateView.java
+++ src/plugin/views/ValidateView.java
@@ -1,32 +1,79 @@
 package plugin.views;
 
+import husacct.common.dto.ViolationDTO;
+
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Frame;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
 import plugin.controller.PluginController;
 
 public class ValidateView extends ViewPart {
 	private PluginController pluginController;
-
+	private ArrayList<ViolationDTO> violationArrayList = new ArrayList<ViolationDTO>();
+	private JTable violationTable;
+	
 	public ValidateView() {
 	}
-
+	
 	@Override
 	public void createPartControl(Composite parent) {
+		
 		pluginController = PluginController.getInstance();
 		Composite composite = new Composite(parent, SWT.EMBEDDED);	
 		Frame frame = SWT_AWT.new_Frame(composite);
 		frame.setLayout(new BorderLayout());
-		frame.add(pluginController.getValidateFrame().getRootPane(), BorderLayout.CENTER);
+		
+		String[] columnNames = {"From",
+                "To",
+                "Line number",
+                "Dependency type"};
+		
+		violationArrayList = pluginController.getViolations();
+		for(ViolationDTO dto: violationArrayList){
+			System.out.println(dto.logicalModuleFrom);
+		}
+		
+		Object[][] data = { {"views.Display", "models.Log", "8", "Import"}, {"views.Display", "models.Storage", "9", "Import"} };
+		
+		violationTable = new JTable(data, columnNames);
+		
+		violationTable.addMouseListener(new MouseAdapter() {
+			  public void mouseClicked(MouseEvent e) {
+			      	System.out.println("(" + violationTable.getValueAt(violationTable.getSelectedRow(), 0) + ".java:" +violationTable.getValueAt(violationTable.getSelectedRow(), 2)+")");
+			      	try {
+						//PlatformUI.getWorkbench().getBrowserSupport().createBrowser("1").openURL(new URL("(" + violationTable.getValueAt(violationTable.getSelectedRow(), 0) + ".java:" +violationTable.getValueAt(violationTable.getSelectedRow(), 2)+")"));
+					} catch (PartInitException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (MalformedURLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+			  }
+			});
+
+		frame.add(new JScrollPane(violationTable), BorderLayout.CENTER);
 		createButton(frame);
 		frame.validate();
 		frame.repaint();
