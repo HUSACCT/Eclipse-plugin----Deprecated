af9ceeca9a3b4931fc5787c624398a097587f0ce
stefan89 <stefan89_@hotmail.com>
2012-05-12 13:29:18 +0200

    Graphics view opgesplits in 2 losse views

---
+++ src/plugin/views/GraphicsAnalysedArchitectureView.java
@@ -0,0 +1,40 @@
+package plugin.views;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JInternalFrame;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+import plugin.controller.PluginController;
+
+public class GraphicsAnalysedArchitectureView extends ViewPart {
+	private Frame frame;
+	
+	public GraphicsAnalysedArchitectureView() {
+
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		PluginController pluginController = PluginController.getInstance();
+		Composite composite = new Composite(parent, SWT.EMBEDDED);	
+		frame = SWT_AWT.new_Frame(composite);
+		frame.setLayout(new BorderLayout());
+		frame.add(pluginController.getGraphicsAnalysedArchitecture().getRootPane(), BorderLayout.CENTER);
+		frame.validate();
+		frame.repaint();
+		parent.setParent(composite);
+	}
+	
+	@Override
+	public void setFocus() {
+		
+	}
+
+}
