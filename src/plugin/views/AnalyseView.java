a6f5bf44bf87285ba3606f251010a7e03f5ab55d
stefan89 <stefan89_@hotmail.com>
2012-05-12 14:29:49 +0200

    Kleine refactoring

--- src/plugin/views/AnalyseView.java
+++ src/plugin/views/AnalyseView.java
@@ -2,20 +2,10 @@ package plugin.views;
 
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
-
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Frame;
-import java.awt.Panel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileFilter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.part.ViewPart;
 import plugin.controller.PluginController;
 
 public class AnalyseView extends ViewPart {
