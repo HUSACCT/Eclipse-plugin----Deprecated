7ddfdac05fc960dd0801f9747da7f797e0fd01ec
stefan89 <stefan89_@hotmail.com>
2012-05-15 12:55:33 +0200

    Views kun je vanaf nu verslepen naar een gewenste locatie

--- src/plugin/perspective/HusacctPerspectiveFactory.java
+++ src/plugin/perspective/HusacctPerspectiveFactory.java
@@ -4,20 +4,23 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
-
 public class HusacctPerspectiveFactory implements IPerspectiveFactory {
 	@Override
 	public void createInitialLayout(IPageLayout myLayout) {
 		myLayout.setEditorAreaVisible(false);
-		IFolderLayout topLeftFolder = myLayout.createFolder("topLeft", IPageLayout.LEFT, 0.20f, myLayout.getEditorArea());
+				
+		IFolderLayout topLeftFolder = myLayout.createFolder("topLeft", IPageLayout.LEFT, 0.18f, myLayout.getEditorArea());
 		topLeftFolder.addView("org.eclipse.jdt.ui.PackageExplorer");
-		IFolderLayout bottomLeftFolder = myLayout.createFolder("bottomLeft", IPageLayout.BOTTOM, 0.5f, "topLeft");
+		
+		IFolderLayout bottomLeftFolder = myLayout.createFolder("bottomLeft", IPageLayout.BOTTOM, 0.7f, "topLeft");
 		bottomLeftFolder.addView("plugin.views.StateView");
+				
 		IFolderLayout rightFolder = myLayout.createFolder("RIGHT", IPageLayout.RIGHT, 0.25f, myLayout.getEditorArea());
 		rightFolder.addView("plugin.views.DefineView");
 		rightFolder.addView("plugin.views.AnalyseView");
 		rightFolder.addView("plugin.views.ValidateView");
 		rightFolder.addView("plugin.views.GraphicsAnalysedArchitectureView");
 		rightFolder.addView("plugin.views.GraphicsDefinedArchitectureView");
+		
 	}
 }
