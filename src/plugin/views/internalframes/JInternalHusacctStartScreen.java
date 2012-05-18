5f8a9e590824e58c3de95b1e08265112dced6804
stefan89 <stefan89_@hotmail.com>
2012-05-18 20:42:12 +0200

    Helpdeel van mainview aangepast

--- src/plugin/views/internalframes/JInternalHusacctStartScreen.java
+++ src/plugin/views/internalframes/JInternalHusacctStartScreen.java
@@ -12,13 +12,14 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import org.eclipse.core.runtime.FileLocator;
 import plugin.Activator;
 
 public class JInternalHusacctStartScreen extends JInternalFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel panelStart, panelHelp;
-	private JLabel labelStartInfo;
+	private JLabel labelStartInfo, labelHelpInfo;
 	private JTextArea textAreaHelpInfo;
 	private GridLayout gridLayoutStartPanel, gridLayoutHelpPanel;
 	private BufferedImage bufferedImage;
@@ -68,14 +69,62 @@ public class JInternalHusacctStartScreen extends JInternalFrame {
 	
 	public void initializeHelpPane(){
 		gridLayoutHelpPanel = new GridLayout(10,2);
-
+		labelHelpInfo = new JLabel(" HERE CAN YOU FIND INFORMATION ABOUT THE WORKFLOW OF THE HUSACCT ECLIPSE PLUGIN");
 		textAreaHelpInfo = new JTextArea();
-		textAreaHelpInfo.setText("--> Hier komt nog hele leuke informatie " + "\n" + "iets wat nuttiger is dan wat er nu staat");
-		textAreaHelpInfo.setEditable(false);
-		
+		textAreaHelpInfo.setText	("test"
+				
+						);
+		//textAreaHelpInfo.setEditable(false);
+        JTextPane textPane = new JTextPane();
+//        Font font = new Font("Cambria", Font.PLAIN, 13);
+//        textPane.setFont(font);
+        
+        textPane.setForeground(Color.BLACK);
+       // textPane.setContentType("text/html");
+        
+        textPane.setText( 
+        		"State of the process:  \n" +
+        		"Within the “StateView” you can see how far you are in the process and what steps you can take. \n" +
+        		"In this view you can see if a step is performed, ready to do or not yet available \n" +
+        		"\n" +
+
+        		"Steps: \n" +
+        		"\n"+
+        		
+        		"Define logical  architecture: 	\n" +
+        		"You can define the logical architecture in the “Define” view. \n" +
+        		"Within this view you can also import and export the logical architecture. \n" +
+        		"\n"+
+        		
+        		"Analyse a Java project within Eclipse: \n" +
+        		"You can analyse a Java Project within Eclipse by right-click at a Java Project "+
+        		"in the “Package Explorer” view and click at “HUSACCT – Analyse Project”."+
+        		"When de Java project analyse is completed, you can see the analysed application within the “Analyse” View .\n" +
+        		"\n"+
+        		
+        		"Validate: \n" +
+        		"Wanneer de architectural rules gedefinieerd zijn en het Java project geanalyseerd is, \n" +
+        		"zal de “StateView” laten zien dat u kunt Valideren. \n" +
+        		"U kunt dit doen door de Validate View te openen en binnen deze view op de knop “Validate” te klikken.\n" +
+        		"Als u dubbelklikt op een violation zal de source geopend worden op de regel waar de violation zich voordoet.\n" +
+        		"\n"+
+        		
+        		"Graphical representation:	\n" +
+        		"Het is mogelijk om een grafische representatie van de analysed architecture te laten zien. \n" +
+        		"Dit kunt u doen door de “Graphics – Analysed” view te openen.\n" +
+        		"Ook is er een graphical representation van de defined architecture te bekijken. \n" +
+        		"Dit doet u door de “Graphics – Defined” view te openen.\n"
+        		
+        
+        
+        );
+        textAreaHelpInfo.setSize(500, 500);
+        
 		panelHelp = new JPanel();
 		panelHelp.setBackground(Color.WHITE);
 		panelHelp.setLayout(gridLayoutHelpPanel);
-		panelHelp.add(textAreaHelpInfo);
+		panelHelp.add(labelHelpInfo);
+		//panelHelp.add(textAreaHelpInfo);
+		panelHelp.add(textPane);
 	}
 }
