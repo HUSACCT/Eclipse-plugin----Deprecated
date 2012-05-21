763061332fa82978d8327661846e79585a8b1b69
stefan89 <stefan89_@hotmail.com>
2012-05-21 20:22:57 +0200

    JInternalframe gemaakt die weergeven kan worden als een view nog niet gebruikt mag worden door de gebruiker

---
+++ src/plugin/views/internalframes/JInternalHusacctNotAvailableScreen.java
@@ -0,0 +1,32 @@
+package plugin.views.internalframes;
+
+import java.awt.Color;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+
+public class JInternalHusacctNotAvailableScreen extends JInternalFrame {
+	private static final long serialVersionUID = 1L;
+	private JTextPane textPaneInformation;
+	private JPanel jPanel;
+	
+	public JInternalHusacctNotAvailableScreen() {
+
+		textPaneInformation = new JTextPane();
+		jPanel = new JPanel();
+		setBounds(50, 50, 200, 400);
+		setResizable(true);
+		setClosable(true);
+		setMaximizable(true);
+		setIconifiable(true);
+		setTitle("Husacct");
+		setVisible(true);
+		
+		textPaneInformation.setText("THIS SCREEN IS NOT AVAILABLE YET. /n" +
+		"In the Stateview you can see when it is available");
+	
+		jPanel.add(textPaneInformation);
+		jPanel.setBackground(Color.WHITE);
+		getContentPane().add(jPanel);
+	}
+}
